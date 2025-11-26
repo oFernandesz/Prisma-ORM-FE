ccdb4348fdcf67492f79468e83102b6528175938
oFernandes <fernandesocaio@gmail.com>
2025-11-26 14:31:37 -0300

    feat: correção better auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // tem que ser a porta onde o backend está rodando
+  baseURL: "http://localhost:3000", 
 });
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,19 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+
 import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-  baseURL: "http://localhost:3000",
-  appName: "LastDance",
-  secret: process.env.BETTER_AUTH_SECRET || "your-secret-key",
-  database: prismaAdapter(prisma, { provider: "sqlite" }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  plugins: [nextCookies()],
+    database: prismaAdapter(prisma, {
+        provider: "sqlite",
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    plugins: [nextCookies()]
+
 });
